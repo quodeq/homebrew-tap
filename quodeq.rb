f6640050857508942a508c8004dd67d4a477f3e5
Victor Purcallas Marchesi <vpurcallas@gmail.com>
2026-05-10 08:10:21 +0000

    chore: update formula to v1.1.1

--- quodeq.rb
+++ quodeq.rb
@@ -3,8 +3,8 @@ class Quodeq < Formula
 
   desc "AI-powered source code quality evaluation platform"
   homepage "https://github.com/quodeq/quodeq"
-  url "https://files.pythonhosted.org/packages/source/q/quodeq/quodeq-1.1.0.tar.gz"
-  sha256 "4d9ab4e26ae9f5fc1f4b86714ec2aeef5900905a255c0e171eab06d51ad2c6d9"
+  url "https://files.pythonhosted.org/packages/source/q/quodeq/quodeq-1.1.1.tar.gz"
+  sha256 "014eee8520b4790789359f1dd0fab09fc6d6b77d48f50211567522d5240c08d8"
   license "MIT"
 
   depends_on "python@3.12"
