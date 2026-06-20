b308b7d81e2be196d9eb9dd1539c0e454cf1fc15
Victor Purcallas Marchesi <vpurcallas@gmail.com>
2026-06-20 19:38:14 +0000

    chore: update formula to v1.4.0

--- quodeq.rb
+++ quodeq.rb
@@ -3,11 +3,12 @@ class Quodeq < Formula
 
   desc "AI-powered source code quality evaluation platform"
   homepage "https://github.com/quodeq/quodeq"
-  url "https://files.pythonhosted.org/packages/44/81/6841bab30f72f3884aca86067da0d36439928a2113d98c8635e943ba98fb/quodeq-1.3.0.tar.gz"
-  sha256 "28a78700eb2d18ccae768abb8214ebf9a00f5fb786c27d0b7d6be8a132c86d4e"
+  url "https://files.pythonhosted.org/packages/fb/6d/4e1e960d238f8c56c590a78273ff6693f69e98028792c63ca5a6cc8104ac/quodeq-1.4.0.tar.gz"
+  sha256 "a54fd25d447b378f112403a071b1b7a76a29dc02d9371c7f94f23734b07ce6bf"
   license "MIT"
 
   depends_on "python@3.12"
+  depends_on "node"
 
   def install
     virtualenv_install_with_resources
